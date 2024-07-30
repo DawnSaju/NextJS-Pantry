b961b103afb6084d7f36ede8063ce164fe14baae
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-07-30 13:28:28 +0530

    V2

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { getUserSession } from '@/lib/session'
-
-export default async function Home() {
-  const user = await getUserSession()
-  return <main className="">{JSON.stringify(user)}</main>
-}
