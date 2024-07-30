3402f11aa275f0d9d013f0bd2f1e43c797b805e5
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-07-30 14:19:28 +0530

    v3

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBto0fsHFZzEUBZUQ4JcMhOaKk39MUnrqw",
+  authDomain: "pantry-app-95fce.firebaseapp.com",
+  projectId: "pantry-app-95fce",
+  storageBucket: "pantry-app-95fce.appspot.com",
+  messagingSenderId: "838999789452",
+  appId: "1:838999789452:web:a589a62d09e784460590a9"
+};
+// Initialize Firebase only if it's not already initialized
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firebaseConfig, firestore };
