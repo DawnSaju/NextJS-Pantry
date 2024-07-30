1771542f1f8c9e9cf2c14121113629ef62b63baf
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-07-30 14:39:53 +0530

    v3

--- app/layout.js
+++ app/layout.js
@@ -2,20 +2,22 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 import { firebaseConfig } from "../firebase";
 import { FirebaseAppProvider } from "reactfire";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <html lang="en">
-        <body className={inter.className} style={{ overflowX: 'hidden' }}>
-          {children}
-        </body>
-      </html>
+      <SessionProvider>
+        <html lang="en">
+          <body className={inter.className} style={{ overflowX: 'hidden' }}>
+            {children}
+          </body>
+        </html>
+      </SessionProvider>
     </FirebaseAppProvider>
   );
 }
