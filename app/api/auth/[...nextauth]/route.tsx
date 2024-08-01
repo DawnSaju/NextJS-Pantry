775d345b59e4b14eac37402f2e4bb12667dd228e
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-08-02 01:41:33 +0530

    v6

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -8,8 +8,6 @@ import GithubProvider from 'next-auth/providers/github'
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
 
-console.log(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET)
-
 const authOption: NextAuthOptions = {
     session: {
       strategy: 'jwt',
@@ -25,14 +23,32 @@ const authOption: NextAuthOptions = {
       }),
     ],
     callbacks: {
-        async jwt({ token, account }) {
-          if (account) {
-            token.accessToken = account.access_token
+      async jwt({ token, account, profile }) {
+        if (account) {
+          token.accessToken = account.access_token;
+  
+          if (profile?.email) {
+            const user = await prisma.user.findUnique({
+              where: { email: profile.email },
+            });
+            if (user) {
+              token.id = user.id;
+              token.image = user.image;
+              token.name = user.name;
+            }
           }
-          return token
-        },
-      }
-    }
+        }
+        return token;
+      },
+      
+      async session({ session, token }) {
+        if (token) {
+          session.user = token
+        }
+        return session;
+      },
+    },
+  }
     
 const handler = NextAuth(authOption)
 export { handler as GET, handler as POST }
