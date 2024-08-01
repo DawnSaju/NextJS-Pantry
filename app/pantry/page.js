1692bb9a90550242084f67a5f68c6def71de7c42
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-08-02 02:31:20 +0530

    v6

--- app/pantry/page.js
+++ app/pantry/page.js
@@ -421,6 +421,7 @@ import { update } from "firebase/database";
 import { useSession, signIn } from "next-auth/react";
 import { getUserSession } from '../../lib/session'
 import Sidebar from "../sidebar";
+import OpenAI from "openai";
 
 export default function Home() {;
   const [pantry, setPantry] = useState([]);
@@ -438,15 +439,17 @@ export default function Home() {;
   const [searchParam, setSearchParam] = useState("");
   const [param, setParam] = useState("");
   const [user, setUser] = useState(null);
+  const [userId, setUserId] = useState(null);
   const [loading, setLoading] = useState(true);
   const [pantryItems, setPantryItems] = useState([]);
   const [currDate, setCurrDate] = useState(new Date());
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState(null);
 
   const { data: session, status } = useSession();
 
   useEffect(() => {
     if (status === "authenticated") {
-      console.log(session.user);
       setUser(session.user);
     } else if (status === "unauthenticated") {
       signIn();
@@ -484,9 +487,55 @@ export default function Home() {;
     }
   }
 
+  const suggestRecipe = async () => {
+    const openai = new OpenAI({ 
+      apiKey: process.env.OPENAI_API_KEY, 
+      baseURL: process.env.OPENAI_BASE_URL, 
+    });
+
+    const jsonPantry = pantryItems.map(item => item.name).join(", ");
+
+    const prompt = 
+    `
+    Give me the suggested recipe based on the pantries that I have on me: ${jsonPantry}
+    `
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: process.env.BASE, },
+        { role: "user", content: prompt, },
+      ],
+      model: process.env.MODEL,
+    });
+  
+    setResponse(completion.choices[0].message.content);
+
+    if (session.user.id) {
+      setUserId(session.user.id);
+    } else {
+      setUserId(session.user.name);
+    }
+    const userDocRef = doc(firestore, `users/${userId}`);
+    const pantryRef = collection(userDocRef, 'pantry');
+    const pantrySnapshot = await getDocs(pantryRef);
+    const items = pantrySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    const userPantryItems = items.map(item => item.name).join(", ");
+    const recipeData = {
+      recipe: completion.choices[0].message.content,
+      pantryItems: pantryItems,
+      createdAt: new Date().toISOString()
+    };
+    const recipeCollectionRef = collection(firestore, `users/${userId}/recipes`);
+    await addDoc(recipeCollectionRef, recipeData);
+    alert("Recipe suggestion saved successfully.");
+  };
+
   const addItem = async (itemName, itemQuantity, expiryDate) => {
     if (status === "authenticated") {
-      const userId = session.user.id;
+      if (session.user.id) {
+        setUserId(session.user.id);
+      } else {
+        setUserId(session.user.name);
+      }
   
       if (!itemName || !itemQuantity || !expiryDate) {
         console.error("Item name, quantity, and expiry date are required");
@@ -535,7 +584,11 @@ export default function Home() {;
     }
   
     try {
-      const userId = session.user.id; 
+      if (session.user.id) {
+        setUserId(session.user.id);
+      } else {
+        setUserId(session.user.name);
+      }
       const pantryCollectionRef = collection(firestore, `users/${userId}/pantry`);
       const q = query(pantryCollectionRef, where("name", "==", itemName));
       const querySnapshot = await getDocs(q);
@@ -575,7 +628,11 @@ export default function Home() {;
     }
 
     try {
-      const userId = session.user.id;
+      if (session.user.id) {
+        setUserId(session.user.id);
+      } else {
+        setUserId(session.user.name);
+      }
       const pantryCollectionRef = collection(firestore, `users/${userId}/pantry`);
       const q = query(pantryCollectionRef, where("name", "==", item));
       const querySnapshot = await getDocs(q);
@@ -596,7 +653,11 @@ export default function Home() {;
 
   const updatePantry = async () => {
     if (status === "authenticated") {
-      const userId = session.user.id;
+      if (session.user.id) {
+        setUserId(session.user.id);
+      } else {
+        setUserId(session.user.name);
+      }
       const userDocRef = doc(firestore, `users/${userId}`);
       const pantryRef = collection(userDocRef, 'pantry');
       const pantrySnapshot = await getDocs(pantryRef);
@@ -613,7 +674,11 @@ export default function Home() {;
     if (status === "authenticated" && session) {
       const fetchPantryItems = async () => {
         try {
-          const userId = session.user.id;
+          if (session.user.id) {
+            setUserId(session.user.id);
+          } else {
+            setUserId(session.user.name);
+          }
           const userDocRef = doc(firestore, `users/${userId}`);
           const userDoc = await getDoc(userDocRef);
 
@@ -621,9 +686,6 @@ export default function Home() {;
             const pantryRef = collection(userDocRef, 'pantry');
             const pantrySnapshot = await getDocs(pantryRef);
             const items = pantrySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-            console.log('Pantry items:', items);
-
             setPantryItems(items);
           } else {
             console.log('No user document found');
@@ -637,14 +699,14 @@ export default function Home() {;
 
       fetchPantryItems();
     }
-  }, [status, session]);
+  }, [status, session, userId]);
   return (
     <div className="flex-1">
       <Sidebar />
-      <main className="flex justify-center items-center h-screen bg-gradient-to-b from-green-100 to-green-200 p-8">
-        <div className="max-w-6xl mx-auto">
+      <main className="main flex justify-center items-center bg-gradient-to-b from-green-100 to-green-200">
+        <div className="max-w-6xl mx-auto p-2">
           <div className="bg-white rounded-lg shadow-lg p-8">
-            <p className="text-lg mb-6 text-gray-600">Manage your pantry items with ease. Add, update, remove, and search your pantry items all in one place.</p>
+            <p className="text-lg mb-6 text-gray-600 p-8">Manage your pantry items with ease. Add, update, remove, and search your pantry items all in one place.</p>
             <div className="flex flex-col lg:flex-row gap-8">
               <div className="flex-1 bg-white p-6 rounded-lg shadow-md">
                 <h2 className="text-2xl font-semibold mb-4 text-gray-800">Add New Item</h2>
@@ -682,8 +744,8 @@ export default function Home() {;
                 <input
                   type="text"
                   placeholder="Search..."
-                  value={param}
-                  onChange={(e) => setParam(e.target.value)}
+                  value={searchParam}
+                  onChange={(e) => setSearchParam(e.target.value)}
                   className="w-full p-3 mb-4 border border-gray-300 rounded-lg"
                 />
                 <ul className="space-y-4">
@@ -717,6 +779,32 @@ export default function Home() {;
                 </ul>
               </div>
             </div>
+            <div className="flex-1 bg-white p-6 rounded-lg shadow-md">
+                <ul className="space-y-4">
+=                  {response ? (
+                    <div className="bg-gray-200 p-4 rounded-lg" dangerouslySetInnerHTML={{ __html: response }} />
+                  ) : (
+                    <li className="text-gray-500">No recipe found.</li>
+                  )}
+                </ul>
+              </div>
+              <div className="flex justify-between items-center p-4 border border-gray-300 rounded-lg bg-gray-50">
+                  <div className="flex space-x-2 w-full">
+                    <input
+                      type="text"
+                      placeholder="Type your message here..."
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                      className="w-full p-3 mb-4 border border-gray-300 rounded-lg"
+                    />
+                    <button
+                      onClick={() => suggestRecipe()}
+                      className="p-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
+                    >
+                      Send
+                    </button>
+                  </div>
+                </div>
           </div>
         </div>
       </main>
