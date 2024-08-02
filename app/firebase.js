049c2ce647862db6a9704fe5c5a5d8bfe6ad5267
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-08-02 22:28:39 +0530

    v6

--- app/firebase.js
+++ app/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage, ref, uploadBytes  } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase only if it's not already initialized
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-export { firebaseConfig, firestore };
+const storage = getStorage(app);
+export { firebaseConfig, firestore, storage, ref, uploadBytes  };
