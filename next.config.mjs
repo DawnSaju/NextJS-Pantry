c3317932e6cd6b08ec8a8ebefdb3911743cbada6
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-08-04 16:33:22 +0530

    v10

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
-import TerserPlugin from 'terser-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true, 
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.optimization.minimizer.push(new TerserPlugin({
-                parallel: true,
-                terserOptions: {
-                    compress: {
-                        drop_console: true, 
-                    },
-                },
-            }));
-        }
-        return config;
-    },
+    swcMinify: true,
 };
 
 export default nextConfig;
